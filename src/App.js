f3f6d44cd3d8c8a4017db1557a0f24c9e84a8e3d
ArtBar9981 <scorp9981@gmail.com>
2022-12-13 17:35:34 +0400

    all work is done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './Components/Main/Main';
 
 function App() {
   return (
-      <Main />
+    <Main />
   );
 }
 
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import './Main.css'
+import React, { useState } from 'react';
+import './Main.css';
 
 export default function Main() {
-    const [value, setValue] = useState([]);
-    const [input, setInput] = useState('');
+  const [value, setValue] = useState([]);
+  const [input, setInput] = useState('');
 
-    const url = `https://giphy.p.rapidapi.com/v1/gifs/search?api_key=UOGLSxkTAElybRXu73xlZd15DjKMWah4&q=${input}`
-  
-    const tagHandler = (e) => {
-      setInput(e.target.value);
-    };
-  
-    const tagSubmit = (e) => {
-      e.preventDefault();
+  const url = `https://giphy.p.rapidapi.com/v1/gifs/search?api_key=UOGLSxkTAElybRXu73xlZd15DjKMWah4&q=${input}`;
 
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': 'f5e919abe7msh55111e9f3cea076p192978jsn4d0a02cb4ae5',
-          'X-RapidAPI-Host': 'giphy.p.rapidapi.com',
-        },
-      };
-  
-      fetch(url, options)
-        .then((res) => res.json())
-        .then((res) => setValue(res.data))
-        .catch((err) => console.error(err));
+  const tagHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const tagSubmit = (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'f5e919abe7msh55111e9f3cea076p192978jsn4d0a02cb4ae5',
+        'X-RapidAPI-Host': 'giphy.p.rapidapi.com',
+      },
     };
-  
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((res) => setValue(res.data))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="container-logo">
           <div className="logo">
             GIPHY
-            </div>
+          </div>
         </div>
         <div className="container-text">
           Введите тему GIF, которые вы хотите увидеть (На английском)
@@ -45,11 +45,9 @@ export default function Main() {
       </div>
       <div className="container-gif">
         {value?.map((el) => (
-            <>
-            <img className="gif" key={el.id} src={el.images.original.url} alt="" />
-            </>
+          <img className="gif" key={el.id} src={el.images.original.url} alt="" />
         ))}
       </div>
-      </div>
-  )
+    </div>
+  );
 }
